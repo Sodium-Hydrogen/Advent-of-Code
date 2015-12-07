eafa30b4f857462e54de2715e414b08dd76b7371
Michael Julander <1110mdj@gmail.com>
2015-12-07 12:20:36 -0700

    Update Day_5_Part_2.js

--- Day_5_Part_2.js
+++ Day_5_Part_2.js
@@ -1,6 +1,6 @@
-//var html = document.getElementsByTagName('pre')[0];
-//input = html.innerHTML;
-var input = "qjhvhtzxzqqjkmpb"
+var html = document.getElementsByTagName('pre')[0];
+input = html.innerHTML;
+//var input = "qjhvhtzxzqqjkmpb"
 console.log(input);
 var x = input.split('\n');
 console.log(x);
@@ -9,7 +9,7 @@ var nice = [];
 var naughty = [];
 var niceArray = 0;
 var naughtyArray = 0;
-while (array == x.length - 1){
+while (array < x.length - 1){
   var arraySub = 0;
   var arrayCal = x[array];
   var other = 0;
@@ -19,7 +19,6 @@ while (array == x.length - 1){
     var a = arrayCal[arraySub];
     var b = arrayCal[arraySub + 1];
     var matching = new RegExp(a + b , 'g');
-    console.log(matching);
     if (arrayCal[arraySub] == arrayCal[arraySub + 2]){
       repeat += 1;
     } else {}
@@ -27,21 +26,19 @@ while (array == x.length - 1){
     if (other != null){
       group = 1;
     } else {}
-    console.log(other);
     arraySub += 1;
   }
-  if (repeat >= 1){
+  if (repeat >= 1 && group == 1){
     nice[niceArray] = arrayCal;
     niceArray += 1;
   } else {
     naughty[naughtyArray] = arrayCal;
     naughtyArray += 1;
   }
-  console.log(other);
-  console.log(group);
   array += 1;
 }
 console.log(nice);
 console.log(naughty);
 console.log(nice.length);
 console.log("exit");
+//431
