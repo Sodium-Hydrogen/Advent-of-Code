1f61670823331e4605bc91db4c194c62b5d5fa70
Michael Julander <1110mdj@gmail.com>
2015-12-07 12:55:48 -0700

    Update Day_6_Part_1.js

--- Day_6_Part_1.js
+++ Day_6_Part_1.js
@@ -1,1 +1,12 @@
-
+var html = document.getElementsByTagName('pre')[0];
+input = html.innerHTML;
+console.log(input);
+var other = input.split('\n');
+console.log(other);
+var x = 0;
+var y = 0;
+var array = 0;
+while (array < x.length - 1){
+  
+}
+console.log('exit')
