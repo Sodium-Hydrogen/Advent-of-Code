1348d14ba2570817683966519ff8f5413527b6e6
Michael Julander <1110mdj@gmail.com>
2015-12-08 10:33:19 -0700

    Update Day_6_Part_1.js

--- Day_6_Part_1.js
+++ Day_6_Part_1.js
@@ -6,7 +6,27 @@ console.log(other);
 var x = 0;
 var y = 0;
 var array = 0;
-while (array < x.length - 1){
-  
+var stuff = 0;
+var lights = [];
+var i = [];
+var m = [];
+while(array <= 999999){
+  lights[array] = 0;
+  array += 1;
+}
+array = 0;
+console.log(lights);
+while (array < other.length - 1){
+  stuff = other[array].split(" ");
+  console.log(stuff);
+  if (stuff[0] == "toggle"){
+    i = stuff[1].split(",");
+    m = stuff[3].split(",");
+    i.push(m[0]);
+    i.push(m[1]);
+    i.parseInt()
+    console.log(i);
+  }
+  array += 1;
 }
 console.log('exit')
