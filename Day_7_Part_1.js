9c23b1e83434fa8cc68cbc9b0c2bef210131e3fc
Michael Julander <1110mdj@gmail.com>
2015-12-10 14:38:06 -0700

    Update Day_7_Part_1.js

--- Day_7_Part_1.js
+++ Day_7_Part_1.js
@@ -1,1 +1,12 @@
-
+var html = document.getElementsByTagName('pre')[0];
+input = html.innerHTML;
+var x = input.split('\n');
+console.log(x);
+var i = 0;
+var array = [];
+var circut = [];
+while (i < x.length - 1){
+  array = x[i].split(" ");
+  
+  i++;
+}
